39b04675106529bc5a024658706caf417c1fcf0d
Ben Eyal <ben.eyal.89@gmail.com>
2023-12-01 14:44:13 +0200

    Initial commit

---
+++ project.scala
@@ -0,0 +1,4 @@
+//> using scala 3.3.1
+//> using options -deprecation -no-indent -old-syntax -Wunused:all -Wvalue-discard
+//> using resourceDir ./src/main/resources
+//> using dep com.lihaoyi::pprint:0.8.1
